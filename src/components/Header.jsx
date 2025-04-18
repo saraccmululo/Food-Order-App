72b5105dec7228af527431b0fc0c4e4e7c6b9410
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-04-18 12:18:09 -0500

    created Header, Meals, MealItem, Button components

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import Button from './UI/Button';
+import logoImg from '../assets/logo.jpg';
+
+const Header = () => {
+  return (
+   <header id="main-header">
+    <div id="title">
+      <img src={logoImg} alt="A restaurant" />
+      <h1>ReactFood</h1>
+    </div>
+    <nav>
+      <Button textOnly
+      >
+        Cart (0)
+      </Button>
+    </nav>
+   </header>
+  )
+}
+
+export default Header
